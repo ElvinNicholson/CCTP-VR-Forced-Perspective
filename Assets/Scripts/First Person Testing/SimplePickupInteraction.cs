6b9347345e90f2f43ae711811fa703a9255b3bee
Elvin Nicholson <92233862+ElvinNicholson@users.noreply.github.com>
2024-02-13 00:28:29 +0000

    Added system that retrieves object bounding points

--- Assets/Scripts/First Person Testing/SimplePickupInteraction.cs
+++ Assets/Scripts/First Person Testing/SimplePickupInteraction.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SimplePickupInteraction : MonoBehaviour
 {
     [SerializeField] private LayerMask excludePickupLayerMask;
+    [SerializeField] private LayerMask holdingLayerMask;
     [SerializeField] private float distanceTreshold;
     [SerializeField] private float lerpSpeed;
 
@@ -16,10 +17,32 @@ public class SimplePickupInteraction : MonoBehaviour
     private float initialDistance;
     private Vector3 initialScale;
 
+    private List<Vector3> boundingBoxPoints;
+    private List<Vector3> validBoundingBoxPoints;
+    [SerializeField] private int gridSize;
+
+    private void OnDrawGizmos()
+    {
+        if (validBoundingBoxPoints == null)
+        {
+            return;
+        }
+
+        Gizmos.color = Color.red;
+
+        foreach (Vector3 point in validBoundingBoxPoints)
+        {
+            Gizmos.DrawSphere(point, 0.1f);
+        }
+    }
+
     private void Update()
     {
         if (currentObject)
         {
+            boundingBoxPoints = GetBoundingBoxPoints();
+            validBoundingBoxPoints = RaycastPoint();
+
             Ray anchorRay = new Ray(anchor.position, anchor.forward);
             Physics.Raycast(anchorRay, out RaycastHit anchorRayHit, Mathf.Infinity, excludePickupLayerMask);
 
@@ -50,6 +73,7 @@ public class SimplePickupInteraction : MonoBehaviour
     {
         if (pickupObject == null)
         {
+            // Object Dropped
             currentObject.GetComponent<Rigidbody>().isKinematic = false;
             currentObject.GetComponent<Collider>().enabled = true;
             currentObject = null;
@@ -57,6 +81,7 @@ public class SimplePickupInteraction : MonoBehaviour
         }
         else
         {
+            // Object Picked Up
             currentObject = pickupObject;
             anchor = controllerTransform;
             lerpedPosition = currentObject.transform.position;
@@ -65,5 +90,46 @@ public class SimplePickupInteraction : MonoBehaviour
             currentObject.GetComponent<Rigidbody>().isKinematic = true;
             currentObject.GetComponent<Collider>().enabled = false;
         }
+    }
+
+    private List<Vector3> GetBoundingBoxPoints()
+    {
+        List<Vector3> points = new List<Vector3>();
+
+        Vector3 cellSize = currentObject.GetComponent<Renderer>().bounds.size / gridSize;
+
+        // Iterate through grid points
+        for (int x = 0; x < gridSize + 1; x++)
+        {
+            for (int y = 0; y < gridSize + 1; y++)
+            {
+                for (int z = 0; z < gridSize + 1; z++)
+                {
+                    // Calculate grid point position
+                    Vector3 point = currentObject.GetComponent<Renderer>().bounds.min +
+                        new Vector3(x * cellSize.x, y * cellSize.y, z * cellSize.z);
+
+                    points.Add(point);
+                }
+            }
+        }
+
+        return points;
+    }
+
+    private List<Vector3> RaycastPoint()
+    {
+        List<Vector3> validPoints = new List<Vector3>();
+
+        foreach (Vector3 point in boundingBoxPoints)
+        {
+            Vector3 direction = point - transform.position;
+            if (Physics.Raycast(transform.position, direction, Mathf.Infinity, holdingLayerMask))
+            {
+                validPoints.Add(point);
+            }
+        }
+
+        return validPoints;
     }
 }
