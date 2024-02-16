e793cbabfa3bfd4995049169c7c76d7fcdfbd10c
Elvin Nicholson <92233862+ElvinNicholson@users.noreply.github.com>
2024-02-16 16:01:03 +0000

    Experimental collision testing

--- Assets/Scripts/First Person Testing/SimplePickupInteraction.cs
+++ Assets/Scripts/First Person Testing/SimplePickupInteraction.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
 public class SimplePickupInteraction : MonoBehaviour
 {
-    [SerializeField] private LayerMask excludePickupLayerMask;
+    [SerializeField] private LayerMask excludeHoldingLayer;
     [SerializeField] private LayerMask holdingLayerMask;
     [SerializeField] private float distanceTreshold;
     [SerializeField] private float lerpSpeed;
+    [SerializeField] private float rotateSpeed;
 
     private Transform anchor;
     private Vector3 lerpedPosition = Vector3.zero;
@@ -20,8 +23,11 @@ public class SimplePickupInteraction : MonoBehaviour
     private List<Vector3> shapedGridPoints;
     [SerializeField] private int gridSize;
     private Vector3 rayHitPoint;
-    private Vector3 closestPoint;
-    private Vector3 transformPoint;
+    private Vector3 closestGridPoint;
+
+    [SerializeField] private InputActionReference leftMoveAction;
+    [SerializeField] private InputActionReference rightTurnAction;
+    [SerializeField] private InputActionReference rightThumbstickAction;
 
     private void OnDrawGizmos()
     {
@@ -40,10 +46,15 @@ public class SimplePickupInteraction : MonoBehaviour
 
         Gizmos.color = Color.green;
         Gizmos.DrawSphere(rayHitPoint, 0.1f);
-        Gizmos.DrawRay(transform.position, (lastRayHit - transform.position).normalized * 10f);
 
         Gizmos.color = Color.cyan;
-        Gizmos.DrawSphere(transformPoint, 0.1f);
+        Gizmos.DrawSphere(closestGridPoint, 0.1f);
+    }
+
+    private void Start()
+    {
+        rightThumbstickAction.action.Disable();
+        rightThumbstickAction.action.performed += OnThumbstick;
     }
 
     private void Update()
@@ -54,7 +65,7 @@ public class SimplePickupInteraction : MonoBehaviour
             float closestDistance = RaycastGridPoints();
 
             Ray anchorRay = new Ray(anchor.position, anchor.forward);
-            Physics.Raycast(anchorRay, out RaycastHit anchorRayHit, Mathf.Infinity, excludePickupLayerMask);
+            Physics.Raycast(anchorRay, out RaycastHit anchorRayHit, Mathf.Infinity, excludeHoldingLayer);
 
             if (Vector3.Distance(anchorRayHit.point, lastRayHit) > distanceTreshold * anchorRayHit.distance)
             {
@@ -69,7 +80,6 @@ public class SimplePickupInteraction : MonoBehaviour
             float longestBoundDist = Vector3.Distance(bounds.min, bounds.max);
 
             currentObject.transform.position = transform.position + (direction * closestDistance) - (direction * longestBoundDist / 2);
-            transformPoint = currentObject.transform.position;
             float scale = Vector3.Distance(transform.position, currentObject.transform.position) / initialDistance;
             currentObject.transform.localScale = initialScale * scale;
         }
@@ -82,11 +92,13 @@ public class SimplePickupInteraction : MonoBehaviour
             // Object Dropped
             GetDroppedPos();
 
+            rightTurnAction.action.Enable();
+            rightThumbstickAction.action.Disable();
+
             currentObject.GetComponent<Rigidbody>().isKinematic = false;
             currentObject.GetComponent<Collider>().isTrigger = false;
             currentObject = null;
             anchor = null;
-            shapedGridPoints.Clear();
             lastRayHit = Vector3.zero;
         }
         else
@@ -99,6 +111,10 @@ public class SimplePickupInteraction : MonoBehaviour
             initialScale = pickupObject.transform.localScale;
             currentObject.GetComponent<Rigidbody>().isKinematic = true;
             currentObject.GetComponent<Collider>().isTrigger = true;
+
+            leftMoveAction.action.Enable();
+            rightTurnAction.action.Disable();
+            rightThumbstickAction.action.Enable();
         }
     }
 
@@ -150,13 +166,13 @@ public class SimplePickupInteraction : MonoBehaviour
         {
             Vector3 direction = (point - transform.position).normalized;
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, direction, out hit, Mathf.Infinity, excludePickupLayerMask))
+            if (Physics.Raycast(transform.position, direction, out hit, Mathf.Infinity, excludeHoldingLayer))
             {
                 if (closestDistance > hit.distance)
                 {
                     closestDistance = hit.distance;
                     rayHitPoint = hit.point;
-                    closestPoint = point;
+                    closestGridPoint = point;
                 }
             }
         }
@@ -166,14 +182,56 @@ public class SimplePickupInteraction : MonoBehaviour
 
     private void GetDroppedPos()
     {
-        Vector3 direction = (closestPoint - rayHitPoint).normalized;
-        RaycastHit hit;
-        if (Physics.Raycast(rayHitPoint, direction, out hit, Mathf.Infinity, holdingLayerMask))
+        Vector3 closestGridToRayHit = currentObject.GetComponent<Collider>().ClosestPoint(rayHitPoint);
+        float distance = Vector3.Distance(closestGridToRayHit, rayHitPoint);
+        if (distance > 0.1f)
         {
-            Vector3 controllerDirection = (lastRayHit - transform.position).normalized;
-            currentObject.transform.position = currentObject.transform.position + (controllerDirection * hit.distance * 0.75f);
-            float scale = Vector3.Distance(transform.position, currentObject.transform.position) / initialDistance;
-            currentObject.transform.localScale = initialScale * scale;
+            Vector3 moveDirection = (lerpedPosition - transform.position).normalized;
+            currentObject.transform.position = currentObject.transform.position + (moveDirection * distance * 0.5f);
+            float newScale = Vector3.Distance(transform.position, currentObject.transform.position) / initialDistance;
+            currentObject.transform.localScale = initialScale * newScale;
+
+            shapedGridPoints = GetShapedGridPoints(GetBoundingGridPoints());
+            RaycastGridPoints();
+
+            GetDroppedPos();
+        }
+
+        /*
+        Vector3 hitDirection = (rayHitPoint - transform.position).normalized;
+        RaycastHit hit;
+        if (Physics.Raycast(closestGridPoint, hitDirection, out hit, Mathf.Infinity, excludeHoldingLayer))
+        {
+            if (hit.distance < 0.1f)
+            {
+                return;
+            }
+
+            Vector3 moveDirection = (lerpedPosition - transform.position).normalized;
+            currentObject.transform.position = currentObject.transform.position + (moveDirection * hit.distance * 0.5f);
+            float newScale = Vector3.Distance(transform.position, currentObject.transform.position) / initialDistance;
+            currentObject.transform.localScale = initialScale * newScale;
+
+            shapedGridPoints = GetShapedGridPoints(GetBoundingGridPoints());
+            RaycastGridPoints();
+
+            GetDroppedPos();
+        }
+        */
+    }
+
+    private void OnThumbstick(InputAction.CallbackContext context)
+    {
+        Vector2 thumbstickValue = context.ReadValue<Vector2>();
+
+        if (Mathf.Abs(thumbstickValue.x) > 0.5f)
+        {
+            currentObject.transform.Rotate(0f, -thumbstickValue.x * Time.deltaTime * rotateSpeed, 0f, Space.World);
+        }
+
+        if (Mathf.Abs(thumbstickValue.y) > 0.5f)
+        {
+            currentObject.transform.Rotate(Vector3.right, thumbstickValue.y * Time.deltaTime * rotateSpeed);
         }
     }
 }
